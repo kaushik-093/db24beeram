8dc37b70e570bdd7ed363aab9c8d28f1e65d83e1
kaushik-093 <89543300+kaushik-093@users.noreply.github.com>
2021-11-10 21:15:15 -0600

    final commit

--- controllers/Monkey.js
+++ controllers/Monkey.js
@@ -1,8 +1,15 @@
 var Monkey = require('../models/Monkey'); 
  
-// List of all Costumes 
-exports.Monkey_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Monkey list'); 
+// List of all Monkey 
+exports.Monkey_list = async function(req, res) { 
+    try{ 
+        theMonkey = await Monkey.find(); 
+        res.send(theMonkey); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // for a specific Monkey. 
@@ -11,8 +18,24 @@ exports.Monkey_detail = function(req, res) {
 }; 
  
 // Handle Monkey create on POST. 
-exports.Monkey_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Monkey create POST'); 
+exports.Monkey_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Monkey(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.name = req.body.name; 
+    document.color = req.body.color; 
+    document.weight = req.body.weight; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Monkey delete form on DELETE. 
@@ -23,4 +46,17 @@ exports.Monkey_delete = function(req, res) {
 // Handle Monkey update form on PUT. 
 exports.Monkey_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Monkey update PUT' + req.params.id); 
+}; 
+
+// VIEWS 
+// Handle a show all view 
+exports.Monkey_view_all_Page = async function(req, res) { 
+    try{ 
+        theMonkey = await Monkey.find(); 
+        res.render('Monkey', { title: 'Monkey Search Results', results: theMonkey }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
--- routes/Monkey.js
+++ routes/Monkey.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const Monkey_controlers= require('../controllers/Monkey'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Monkey', { title: 'search results for Monkey' });
-});
+router.get('/', Monkey_controlers.Monkey_view_all_Page );
 
 module.exports = router;
