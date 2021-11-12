9e3a8604f05a27fe5bba1ff3e49e4f8fd82f3545
kaushik-093 <89543300+kaushik-093@users.noreply.github.com>
2021-11-11 21:38:16 -0600

    eddited the name

--- controllers/Monkey.js
+++ controllers/Monkey.js
@@ -51,8 +51,22 @@ exports.Monkey_delete = function(req, res) {
 }; 
  
 // Handle Monkey update form on PUT. 
-exports.Monkey_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Monkey update PUT' + req.params.id); 
+exports.Monkey_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Monkey.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.name)  
+               toUpdate.name = req.body.name; 
+        if(req.body.color) toUpdate.color = req.body.color; 
+        if(req.body.weight) toUpdate.weight = req.body.weight; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 
 
 // VIEWS 
